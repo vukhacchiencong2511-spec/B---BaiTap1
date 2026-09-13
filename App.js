42291ec7375e03a7418798ad0d83037462d67cd1
Công <vukhacchiencong2511@gmail.com>
2026-09-13 12:36:38 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import { student } from "./student.js";
+
+const { name, className, major, year } = student;
+
+console.log("Ho ten:", name);
+console.log("Lop:", className);
+console.log("Nganh hoc:", major);
+console.log("Nam hoc:", year);
---
+++ student.js
@@ -0,0 +1,8 @@
+const student = {
+    name: "Nguyen Van A",
+    className: "SE17",
+    major: "Ky thuat phan mem",
+    year: 3
+};
+
+export { student };
