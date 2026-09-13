ad491d873fdea83c9bad244164244be9a42371fa
Công <vukhacchiencong2511@gmail.com>
2026-09-13 12:38:19 +0700

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import { student } from "./student.js";
-
-const { name, className, major, year } = student;
-
-console.log("Ho ten:", name);
-console.log("Lop:", className);
-console.log("Nganh hoc:", major);
-console.log("Nam hoc:", year);
