f427033a5aeb7e9780586233993f6b1630975327
Công <vukhacchiencong2511@gmail.com>
2026-09-13 12:32:50 +0700

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,13 @@
+//hàm tính tổng 2 số
+const sum = (a, b) => a + b;
+
+//hàm tính bình phương
+const square = (a) => a * a;
+
+//kiểm tra số có lớn hơn 10 hay ko
+const greaterThan10 = (a) => a > 10;
+
+//test run
+console.log("Tổng:", sum(5, 3));
+console.log("Bình phương:", square(4));
+console.log("Lớn hơn 10:", greaterThan10(15));
